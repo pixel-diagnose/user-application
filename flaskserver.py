5386cf8b12f9ce3e2a9b32253ae69a3cbaba2d73
SE-Nie <svenja.niehus@gmail.com>
2023-12-01 15:05:40 +0100

    20231201_svenja: inserted react build  with layout changes

--- flaskserver.py
+++ flaskserver.py
@@ -15,7 +15,7 @@ from werkzeug.utils import secure_filename
 from inference import do_inference
 from google.cloud import storage
 from google.auth import compute_engine
-from parse_qdrant_output import generate_signed_url
+from get_cloud_url import generate_signed_url
 from qdrant_stuff import get_similar_embeddings
 import json
 
@@ -53,29 +53,44 @@ def upload_file():
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(file_path)
             # Get only embedding from uploaded picture here
-            embeddings_from_upload = do_inference("img2vec50", os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            embeddings_from_upload = do_inference(
+                "img2vec50", os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            )
             # Query QDRANT
             num_qdrant_results = int(request.form["number_of_results"])
-            requested_image_type = "".join([key for key, value in json.loads(request.form["image_type"]).items() if value])
+            requested_image_type = "".join(
+                [
+                    key
+                    for key, value in json.loads(request.form["image_type"]).items()
+                    if value == "true"
+                ]
+            )
             assert len(requested_image_type) == 1
-            requested_diagnoses = [key.replace("Gliomas","glioma").replace("Meningiomas","men").replace("Metastasis","met") # change because disgnoses are stored differnetly on QDRANT
-                                    for key, value in  json.loads(request.form["cancer_types"]).items() 
-                                    if value
-                                    ]
-            results = {diagnose:generate_signed_url(
-                                            get_similar_embeddings(
-                                                search_vector=embeddings_from_upload,
-                                                collection_name="resnet50_imagenet_embeddings",
-                                                num_results=num_qdrant_results,
-                                                filter_diagnose=[diagnose],
-                                                filter_image_type=requested_image_type
-                                                )
-                                            )
-                        for diagnose in requested_diagnoses}
+            requested_diagnoses = [
+                key.replace("Gliomas", "glioma")
+                .replace("Meningiomas", "men")
+                .replace(
+                    "Metastasis", "met"
+                )  # change because disgnoses are stored differnetly on QDRANT
+                for key, value in json.loads(request.form["cancer_types"]).items()
+                if value == "true"
+            ]
+            results = {
+                diagnose: generate_signed_url(
+                    get_similar_embeddings(
+                        search_vector=embeddings_from_upload,
+                        collection_name="resnet50_imagenet_embeddings",
+                        num_results=num_qdrant_results,
+                        filter_diagnose=[diagnose],
+                        filter_image_type=requested_image_type,
+                    )
+                )
+                for diagnose in requested_diagnoses
+            }
             # Return JSON from QDRANT
             print("-------with url--------")
             print(results)
-            
+
             return jsonify(results)
 
 
